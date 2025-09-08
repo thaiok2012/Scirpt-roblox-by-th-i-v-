713401a5dad3cd331a301dab7ff041584c3e38dc
thaiok2012 <Vuthai610@gmail.com>
2025-09-08 13:12:53 +0700

    Add files via upload

---
+++ Script by Thái Vũ ver 3.lua
@@ -0,0 +1,43 @@
+getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinUp27Vn"))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Thái Vũ",
+         Animation = "Thái Vũ Đẹp Zai"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=83190276951914",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Script của Min gaming"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Min gaming",
+    Callback = function()
+getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinUp27Vn"))()
+    end
